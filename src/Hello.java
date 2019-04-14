138e96645d43f92e33011c3b0235c7369057592a
Saiful Islam <sislamofficial@hotmail.com>
2019-04-14 16:48:03 +0600

    hello world

---
+++ src/Hello.java
@@ -0,0 +1,8 @@
+public class Hello {
+    public static void  main(String[]Args){
+        String name = "Simran";
+        String dialouge = "Ja, Jile Apni Jindegi!";
+        System.out.println(name);
+        System.out.println(dialouge);
+    }
+}
